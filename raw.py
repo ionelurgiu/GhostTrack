1c4ac5b3e18f1ccefaade8098b334f357a865c00
ionelurgiu <ionelurgiu@outlook.com>
2025-06-19 13:16:21 +0300

    Update raw.py

--- raw.py
+++ raw.py
@@ -24,5 +24,12 @@ with SB(uc=True, test=True) as sb:
         sb.sleep(1)
         sb.uc_gui_handle_captcha()
         sb.sleep(1)
+        try:
+            input_field = 'select[id="ageYear"]'
+            sb.uc_click(input_field, reconnect_time=4)
+            rnd = random.randint(1960,2008)
+            sb.uc_gui_write(rnd)
+            input_btn = 'a[id="view_product_page_btn"]'
+            sb.uc_click(input_btn, reconnect_time=4)
         rnd = random.randint(15,600)
         sb.sleep(15)
